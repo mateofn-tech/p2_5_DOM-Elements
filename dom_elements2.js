bcf884fd5cc4d4458ac53ae923d8f28e98fcd6e1
mateofn-tech <mateofn@easdalcoi.es>
2025-11-13 09:24:56 +0100

    Add files via upload

---
+++ dom_elements2.js
@@ -0,0 +1,41 @@
+let input;
+let slider;
+let xOffset = 0;
+let yOffset = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  input = createInput('');
+  input.attribute('placeholder', 'escriu');
+  input.position(10, 10);
+  input.size(250);
+  
+  slider = createSlider(10, 255, 32);
+  slider.position(10, 50);
+  slider.style('width', '250px');
+
+  if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission();
+  }
+}
+
+function draw() {
+  background(0);
+
+  if (slider.value() > 255) {
+    slider.value(0);
+  }
+  
+  xOffset = map(rotationY, -90, 90, -width/2, width/2);
+  yOffset = map(rotationX, -90, 90, -height/2, height/2);
+  
+  fill(255);
+  textSize(slider.value());
+  textAlign(LEFT, CENTER);
+  text(input.value(), 20 + xOffset, height/2 + yOffset);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
